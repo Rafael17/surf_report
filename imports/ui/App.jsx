0def9cb974a81691e2c7b46f9dab23d181510350
Rafael Arcieri <rafa@rafaels-air.gateway.pace.com>
2016-05-19 22:57:26 -0400

    surf report api

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -8,21 +8,25 @@ export default class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      chosen:this.getMarkers()[0],
-      ax:[],
-      shake_capturing:false
-    }
-  }
-
-
-  getMarkers() {
-    return [
+    var markers = [
       {name:'Blacks',src:"/images/blacks.jpg",index:0,spot_id:229},
       {name:'Sunset Cliffs',src:"/images/sunset-cliffs.jpg",index:1,spot_id:224},
       {name:'Pacific Beach',src:"/images/pacific_beach.jpg",index:2,spot_id:226},
-      {name:'La Jolla Shores',src:"/images/la_jolla_shores.jpg",index:3,spot_id:755},
+      {name:'La Jolla Shores',src:"/images/la_jolla_shores.jpg",index:3,spot_id:228},
+      {name:'Ocean Beach Pier',src:"/images/ocean_beach.jpg",index:4,spot_id:225},
+      {name:'Mission Beach',src:"/images/mission_beach.jpg",index:5,spot_id:397},
     ];
+    var m = markers.map((elem,index) => {
+      elem.wave = '';
+      elem.condition = '';
+      return elem;
+    })
+    this.state = {
+      chosen:markers[0],
+      markers:markers,
+      ax:[],
+      shake_capturing:false
+    }
   }
 
 
@@ -33,15 +37,33 @@ export default class App extends Component {
       window.addEventListener('deviceorientation', this.handleDeviceOrientation.bind(this), false);
       window.addEventListener('devicemotion', this.handleDeviceMotion.bind(this), false);
     }
-    this.getMarkers().forEach(this.requestService)
+    this.state.markers.forEach(this.requestService.bind(this))
     
   }
 
   requestService(elem,index){
+    // Using yahoo api to bypass CORS from spitcast
+    var query = encodeURI("select * from html where url='http://api.spitcast.com/api/spot/forecast/"+elem.spot_id)+"'";
+    var yql = "https://query.yahooapis.com/v1/public/yql?q=";
+    var params = "&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+    var url = yql+query+params;
+    this.serverRequest = $.get(url, function(response){this.serviceCallback(index,response)}.bind(this));
+  }
 
-    this.serverRequest = $.get(App.statics.surf_api_url+'/'+elem.spot_id, function (result) {
-      console.log(result);
-    }.bind(this));
+  serviceCallback(index,result) {
+    var d = new Date();
+    var hour = d.getHours();
+    
+    if(result.query.results){
+      var body = JSON.parse(result.query.results.body);
+      var markers = this.state.markers;
+      
+      markers[index].wave = Math.ceil(body[hour].size_ft * 100) / 100;
+      markers[index].condition = body[hour].shape_full;
+      this.setState({
+        markers:markers
+      })
+    }
   }
 
   componentWillUnmount () {
@@ -87,7 +109,7 @@ export default class App extends Component {
     },0)
     
     if((total+dir)>2){
-      alert("Bake n Shake!");
+      alert("Shake and Bake!");
     }
     this.setState({ax:[]});
     this.setState({shake_capturing:false})
@@ -96,21 +118,23 @@ export default class App extends Component {
   handleDeviceOrientation(event) {
     this.setState({alpha:event.alpha})
 
-    const index = Math.floor(event.alpha/(360/this.getMarkers().length));
+    const index = Math.floor(event.alpha/(360/this.state.markers.length));
     if(this.state.chosen.index !== index){
-      this.setState({chosen:this.getMarkers()[index]});
+      this.setState({chosen:this.state.markers[index]});
     }
   }
 
   render() {
     return (
       <div className="container">
-        <div id="beach_name">{this.state.chosen.name}</div>
+        <div id="beach_name">{this.state.chosen.name}
+          <br/>Wave Size: {this.state.chosen.wave}
+          <br/>Conditions: {this.state.chosen.condition}</div>
         <header>
           <h1>Beaches</h1>
         </header>
         <MotionFollowerBackground image={this.state.chosen.src}/>
-        <Compass alpha={this.state.alpha} markers={this.getMarkers()}/>
+        <Compass alpha={this.state.alpha} markers={this.state.markers}/>
       </div>
     );
   }
